1258b287e773c2e30f7032c48f932800161ab6c2
Mauricio Rojas <mrojasb2000@gmail.com>
2020-01-02 18:36:47 -0300

    Refactor using JSX expression.

--- catalog-react/src/components/catalog.js
+++ catalog-react/src/components/catalog.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import './Catalog.css'
 class catalog extends Component {
     render() { 
-        return (<div><h2>Catalog</h2></div>  );
+        let output = <div><h2>Catalog</h2></div>; 
+        return output;
     }
 }
  
