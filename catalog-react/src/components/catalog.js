6a552754dcb9a3da3f369816094c3f2af1440106
Mauricio Rojas <mrojasb2000@gmail.com>
2020-01-02 18:38:35 -0300

    Embed Javascript expression inside of a JSX.

--- catalog-react/src/components/catalog.js
+++ catalog-react/src/components/catalog.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import './Catalog.css'
 class catalog extends Component {
     render() { 
-        let output = <div><h2>Catalog</h2></div>; 
+        let title = "Catalog"
+        let output = <div><h2>{title}</h2></div>; 
         return output;
     }
 }
