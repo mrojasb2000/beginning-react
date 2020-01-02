a3a2ed370def2d4eda36df9893f00b5457645d89
Mauricio Rojas <mrojasb2000@gmail.com>
2020-01-02 18:40:05 -0300

    Javascript expression can be as complex as we need.

--- catalog-react/src/components/catalog.js
+++ catalog-react/src/components/catalog.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './Catalog.css'
 class catalog extends Component {
     render() { 
-        let title = "Catalog"
+        let title = "The Catalog of day " + new Date().toDateString();
         let output = <div><h2>{title}</h2></div>; 
         return output;
     }
