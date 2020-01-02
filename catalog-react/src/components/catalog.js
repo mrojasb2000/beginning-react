fd4d5022acc28133b1766294a4a46cd2005a09dd
Mauricio Rojas <mrojasb2000@gmail.com>
2020-01-02 18:00:13 -0300

    Add catalog basic component.

---
+++ catalog-react/src/components/catalog.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class catalog extends Component {
+    state = {  }
+    render() { 
+        return (<div><h2>Catalog</h2></div>  );
+    }
+}
+ 
+export default catalog;
