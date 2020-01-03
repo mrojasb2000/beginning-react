dbfd692ecc646fbbcbb1c735ca5653d6f65f3b84
Mauricio Rojas <mrojasb2000@gmail.com>
2020-01-03 13:14:33 -0300

    Remove comment catalog component.

--- catalog-react/src/components/catalog.js
+++ catalog-react/src/components/catalog.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
 import './Catalog.css'
+
 class catalog extends Component {
     render() { 
         let title = "The Catalog of day " + new Date().toDateString();
         let output = <div><h2>{title}</h2></div>; 
         let message;
         let today = new Date().getDay();
-        {// This is a comment}
-        {/* This is a comment, too */}
+      
         if (today == 0) {
             message = <div className="sorry">We are closed on Sunday...</div>;
         } else {
             message = <div className="happy">How can we help you?</div>;
         }
-        return message;
+        return message
     }
 }
  
