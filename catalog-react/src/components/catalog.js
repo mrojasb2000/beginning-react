55fe4f4c4c5fb5380f9a7214b600ce2a005e0002
Mauricio Rojas <mrojasb2000@gmail.com>
2020-01-03 13:58:52 -0300

    Using product list component to catalog.

--- catalog-react/src/components/catalog.js
+++ catalog-react/src/components/catalog.js
@@ -1,19 +1,13 @@
 import React, { Component } from 'react';
+import ProductList from './ProductList';
 import './Catalog.css'
 
 class catalog extends Component {
     render() { 
-        let title = "The Catalog of day " + new Date().toDateString();
-        let output = <div><h2>{title}</h2></div>; 
-        let message;
-        let today = new Date().getDay();
-      
-        if (today == 0) {
-            message = <div className="sorry">We are closed on Sunday...</div>;
-        } else {
-            message = <div className="happy">How can we help you?</div>;
-        }
-        return message
+       return <div>
+           <h2>Catalog</h2>
+           <ProductList />
+       </div>
     }
 }
  
