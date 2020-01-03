7e3424e40f715da1efe7e55e143b40c21d4c34c9
Mauricio Rojas <mrojasb2000@gmail.com>
2020-01-03 14:42:36 -0300

    Refactor product component.

--- catalog-react/src/components/Product.js
+++ catalog-react/src/components/Product.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 
 class Product extends Component {
-
+    
     render() { 
+        let item = this.props.item;
         return <li>
-            <h3>{this.props.item.name}</h3>
-            <p>{this.props.item.description}</p>
+            <h3>{item.name}</h3>
+            <p>{item.description}</p>
         </li>;
     }
 }
