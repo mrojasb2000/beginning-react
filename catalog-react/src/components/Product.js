d5a11649ef4f02fa516e112f8e1d4ee1aff2ef28
Mauricio Rojas <mrojasb2000@gmail.com>
2020-01-03 16:04:44 -0300

    Refactor add event onclick for view price.

--- catalog-react/src/components/Product.js
+++ catalog-react/src/components/Product.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 
 class Product extends Component {
-    
+    showPrice() {
+        alert(this.props.item.price);
+    }
     render() { 
         let item = this.props.item;
-        return <li>
+        return <li onClick={() => this.showPrice()}>
             <h3>{item.name}</h3>
             <p>{item.description}</p>
         </li>;
